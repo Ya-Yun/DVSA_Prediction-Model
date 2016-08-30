989859b86907bf13b98ef59ccfa16bd31d777670
Ya-Yun <chenyayun9683@gmail.com>
2016-08-31 07:07:12 +0800

    Update 2_合併重複地址與通報單.R

--- data_ETL/2_合併重複地址與通報單.R
+++ data_ETL/2_合併重複地址與通報單.R
@@ -9,8 +9,11 @@ circular <- read.csv("通報表被害人相對人資料.csv")
 #在重複地址後方填入通報單號(ACTIONID)及其他predictive index
 merge <- sqldf(
     "select repAddress.*, circular.MAIMED, ACTIONID, VDTYPE, SEXID, IDTYPE, OTHERIDTYPE, OCCUPATION, OTHEROCCUPATION, EDUCATION
-    from repAdress left join circular on repAdress.居住完整地址 = circular.居住完整地址"
+    from repAddress left join circular on repAddress.居住完整地址 = circular.居住完整地址"
     )
     
 #得出重複發生親密關係暴力之通報單號
 #此單號將連結TIPVDA量表及其他通報單資訊
+
+
+write.csv(merge, "merge_ActionID_repAddr.csv", row.names = FALSE)
