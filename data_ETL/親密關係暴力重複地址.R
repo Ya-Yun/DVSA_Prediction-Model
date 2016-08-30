5067d576bf3dc7069f4f8e3fd99cc6b4a9f04861
Ya-Yun <chenyayun9683@gmail.com>
2016-08-31 06:40:53 +0800

    Update 親密關係暴力重複地址.R

--- data_ETL/親密關係暴力重複地址.R
+++ data_ETL/親密關係暴力重複地址.R
@@ -8,5 +8,7 @@ dat.count <- dat %>% group_by(個案類型, 居住完整地址, lng, lat) %>% su
 #只取發生兩次以上(含兩次)的親密關係暴力
 dat.count.repAddress <- subset(dat.count, count > 1 & 個案類型 == "婚姻關係暴力")
 
+#刪除地址不完整之資料
+
 
 write.csv(dat.count.repAddress, "repAddress.csv", row.names = FALSE)
